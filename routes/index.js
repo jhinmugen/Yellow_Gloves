baffecf633dc228142e8bd3b4f83748a1a1bbe26
Evangelos Spanos <spanosvagos@gmail.com>
2019-01-17 18:27:34 +0000

    add routes and views for all pages and seperate header for landing page

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,23 @@ var router = express.Router();
 router.get("/", function(req,res){
    res.render("landing"); 
 });
+//menu page
+router.get("/menu", function(req,res){
+   res.render("menu"); 
+});
+//restaurant page
+router.get("/restaurant", function(req,res){
+   res.render("restaurant"); 
+});
+//employees page
+router.get("/employees", function(req,res){
+   res.render("employees"); 
+});
+//contact page
+router.get("/contact", function(req,res){
+   res.render("contact"); 
+});
+
 
 
 module.exports = router;
