32161d85b850661f04f965311b34c7632df71377
Evangelos Spanos <spanosvagos@gmail.com>
2019-01-22 19:41:53 +0000

    google maps embedded

--- public/scripts/generalScript.js
+++ public/scripts/generalScript.js
@@ -4,7 +4,6 @@ $(function () {
     var  os = $('.top-section').offset().top; // pixels to the top of div1
     var  ht = $('.top-section').height(); // height of div1 in pixel
     var sum = os +ht;
-    console.log(scroll + " >" + sum);
     if(scroll > os + ht){
         $("#generalNavbar").addClass('bg-light navbar-light');
         $("#generalNavbar").removeClass('navbar-dark');
